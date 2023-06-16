caa9a1d568db9b16e3d54bf620ce72d399ab39aa
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-06-17 01:36:28 +0900

    Add: lineログイン機能実装 #13

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks"
+  }
 end
---
+++ db/migrate/20230616150803_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+    add_column :users, :name, :string, null:false
+  end
+end
