39fdc20c1a5e40f35bbd47bb0881b162c3198d93
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-06-19 17:57:54 +0900

    Add: オウム返しlinebot作成

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,37 @@
+class LinebotController < ApplicationController
+  require 'line/bot'
+
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+        end
+      end
+      client.reply_message(event['replyToken'], message)
+    end
+    head :ok
+  end
+
+private
+
+# LINE Developers登録完了後に作成される環境変数の認証
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+end
