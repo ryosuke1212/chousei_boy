22ad0db40cb95e66aa646b1b8f707af7849cfea1
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-06-19 19:09:31 +0900

    Fix: Herokuエラーのため修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -3,10 +3,10 @@ class LinebotController < ApplicationController
 
   def callback
     body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
+    # signature = request.env['HTTP_X_LINE_SIGNATURE']
+    # unless client.validate_signature(body, signature)
+    #   error 400 do 'Bad Request' end
+    # end
     events = client.parse_events_from(body)
 
     events.each do |event|
