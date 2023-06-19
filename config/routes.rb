58446b2cb7aafa20cecb4f918788d27bea7abe94
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-06-19 14:58:14 +0900

    Add: bin/rails g controller schedules 実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  resources :schedules, only: [:index, :create, :destroy, :edit, :update]
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks"
   }
