cad337e99361ec2070de345b498e7cd70fff6293
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-06-25 08:08:23 +0900

    Add: FlexMessageの追加 #17

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -25,29 +25,50 @@ class LinebotController < ApplicationController
           end
 
           if event.message['text'] == '予定作成'
+            schedule = nil
             if event['source']['groupId']
               groupId = event['source']['groupId']
-              Schedule.create(line_group_id: groupId)
-              message = {
-                type: 'text',
-                text: '予定を作成しました。'
-              }
+              schedule = Schedule.create(line_group_id: groupId)
             elsif event['source']['userId']
-              Schedule.create(user_id: event['source']['userId'])
+              schedule = Schedule.create(user_id: event['source']['userId'])
+            end
+
+            if schedule
               message = {
                 type: 'text',
                 text: '予定を作成しました。'
               }
+              flex_message = {
+                type: 'flex',
+                altText: 'メッセージを送信しました',
+                contents: read_flex_message(schedule)
+              }
+              client.reply_message(event['replyToken'], [message, flex_message])
             end
-          else
+
+          elsif event.message['text'] == '予定一覧'
+            user_id = event['source']['userId']
+            schedules = Schedule.where(user_id: user_id)
+            schedules.each do |schedule|
+              message = {
+                type: 'text',
+                text: '予定一覧です'
+              }
+              flex_message = {
+                type: 'flex',
+                altText: 'メッセージを送信しました',
+                contents: read_flex_message(schedule)
+              }
+              client.reply_message(event['replyToken'], [message, flex_message])
+            end
             message = {
               type: 'text',
               text: event.message['text']
             }
+            client.reply_message(event['replyToken'], message)
           end
         end
       end
-      client.reply_message(event['replyToken'], message)
     end
     head :ok
   end
@@ -59,5 +80,19 @@ class LinebotController < ApplicationController
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
+  end
+
+  def read_flex_message(schedule)
+    file_path = Rails.root.join('app', 'messages', 'schedule_create_message.json')
+    message = JSON.parse(File.read(file_path))
+
+    message["body"]["contents"][0]["text"] = schedule.start_time.to_s if schedule.start_time
+    message["body"]["contents"][1]["text"] = schedule.title.to_s if schedule.title
+    message["body"]["contents"][2]["text"] = schedule.location.to_s if schedule.location
+    message["body"]["contents"][4]["contents"][0]["contents"][1]["text"] = schedule.description.to_s if schedule.description
+    message["body"]["contents"][4]["contents"][1]["contents"][1]["text"] = schedule.representative.to_s if schedule.representative
+    message["body"]["contents"][4]["contents"][2]["contents"][1]["text"] = schedule.deadline.to_s if schedule.deadline
+
+    message
   end
 end
