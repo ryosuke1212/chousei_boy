dc841f021314ddbbe66e1d20ff2b48f8f46d47a9
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-07-13 00:04:44 +0900

    Fix: Topページの改善

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('turbolinks:load', event => {
 
     const sidebarWrapper = document.getElementById('sidebar-wrapper');
     let scrollToTopVisible = false;
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get '/terms', to: 'static_pages#terms'
+  get '/privacy_policy', to: 'static_pages#privacy_policy'
   post '/callback' => 'linebot#callback'
   resources :schedules, only: [:index, :create, :destroy, :edit, :update, :show], param: :url_token
   devise_for :users, controllers: {
