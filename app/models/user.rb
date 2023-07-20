09c6a8c2613ce552c797bf741610d47cca007e10
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-07-20 20:00:48 +0900

    Fix: user.rbリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,8 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :line_groups
-  
-  def social_profile(provider)
-    social_profiles.select { |sp| sp.provider == provider.to_s }.first
-  end
 
-  def set_values(omniauth)
-    return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
-    credentials = omniauth["credentials"]
-    info = omniauth["info"]
-
-    access_token = credentials["refresh_token"]
-    access_secret = credentials["secret"]
-    credentials = credentials.to_json
-    name = info["name"]
-  end
-
-  def set_values_by_raw_info(raw_info)
-    self.raw_info = raw_info.to_json
-    self.save!
+  def values(omniauth)
+    return if provider.to_s != omniauth['provider'].to_s || uid != omniauth['uid']
   end
 
   devise :database_authenticatable, :registerable,
