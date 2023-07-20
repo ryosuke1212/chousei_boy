a68e042dbd2c4bcd211c66ad15ef603362eee685
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-07-20 18:40:24 +0900

    Fix: omniauth_callbacks_controller.rbリファクタリング

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,24 +4,26 @@ class OmniauthCallbacksController < ApplicationController
   end
 
   private
-  
+
   def basic_action
-    @omniauth = request.env["omniauth.auth"]
+    @omniauth = request.env['omniauth.auth']
     if @omniauth.present?
-      @profile = User.find_or_initialize_by(provider: @omniauth["provider"], uid: @omniauth["uid"])
+      @profile = User.find_or_initialize_by(provider: @omniauth['provider'], uid: @omniauth['uid'])
       if @profile.email.blank?
-        email = @omniauth["info"]["email"] ? @omniauth["info"]["email"] : "#{@omniauth["uid"]}-#{@omniauth["provider"]}@example.com"
-        @profile = current_user || User.create!(provider: @omniauth["provider"], uid: @omniauth["uid"], email: email, name: @omniauth["info"]["name"], password: Devise.friendly_token[0, 20])
+        email = @omniauth['info']['email'] || "#{@omniauth['uid']}-#{@omniauth['provider']}@example.com"
+        @profile = current_user || User.create!(
+          provider: @omniauth['provider'],
+          uid: @omniauth['uid'],
+          email: email,
+          name: @omniauth['info']['name'],
+          password: Devise.friendly_token[0, 20]
+        )
       end
       @profile.set_values(@omniauth)
       sign_in(:user, @profile)
     end
-    #ログイン後のflash messageとリダイレクト先を設定
-    flash[:notice] = "ログインしました"
+    # ログイン後のflash messageとリダイレクト先を設定
+    flash[:notice] = 'ログインしました'
     redirect_to schedules_path
-  end
-
-  def fake_email(uid, provider)
-    "#{auth.uid}-#{auth.provider}@example.com"
   end
 end
