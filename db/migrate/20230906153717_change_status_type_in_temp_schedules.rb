bee09353d183b04ff4328ff06035118640b43b81
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-09-07 23:59:16 +0900

    Fix: PostgreSQLでstatusカラムの型ｗ変更できるように修正

--- db/migrate/20230906153717_change_status_type_in_temp_schedules.rb
+++ db/migrate/20230906153717_change_status_type_in_temp_schedules.rb
@@ -1,5 +1,9 @@
 class ChangeStatusTypeInTempSchedules < ActiveRecord::Migration[6.1]
-  def change
-    change_column :temp_schedules, :status, :integer, default: 0
+  def up
+    change_column :temp_schedules, :status, :integer, using: 'status::integer', default: 0
+  end
+
+  def down
+    change_column :temp_schedules, :status, :string
   end
 end
